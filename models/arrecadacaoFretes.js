5c8aea6fea8067cdafc752bf9566dc1dc113ca98
joaothallys <jlemes796@gmail.com>
2024-12-08 11:13:01 -0300

    feat: correcao arrecadacaoFrete

--- models/arrecadacaoFretes.js
+++ models/arrecadacaoFretes.js
@@ -1,20 +1,19 @@
-// models/arrecadacaoFretes.js
-const db = require('./db');  // Substitua com o arquivo de conexão do banco de dados
+const db = require('./db'); // Arquivo de conexão com o banco de dados
 
 const getArrecadacaoFretes = (estadoId) => {
   const query = `
     SELECT 
-   c.nome_cidade,
-   e.nome_estado,
-   COUNT(f.id_frete) AS quantidade_de_frete,
-   SUM(f.frete_valor) AS valor_total_arrecadado
-FROM Frete f
-JOIN Cidade c ON f.id_destinatario = c.id_cidade
-JOIN Estado e ON c.id_estado = e.id_estado
-WHERE e.id_estado = 1 AND YEAR(f.data_frete) = 2024
-GROUP BY c.id_cidade, e.id_estado
+      c.nome_cidade,
+      e.nome_estado,
+      COUNT(f.id_frete) AS quantidade_de_frete,
+      SUM(f.frete_valor) AS valor_total_arrecadado
+    FROM Frete f
+    JOIN Cidade c ON f.id_destinatario = c.id_cidade
+    JOIN Estado e ON c.id_estado = e.id_estado
+    WHERE e.id_estado = ? AND YEAR(f.data_frete) = 2024
+    GROUP BY c.id_cidade, e.id_estado
   `;
-  return db.query(query, [estadoId]);
+  return db.query(query, [estadoId]); // Passando o estadoId como parâmetro
 };
 
 module.exports = { getArrecadacaoFretes };
